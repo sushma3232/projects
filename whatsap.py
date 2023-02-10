ec590c0f1b108fef38def184dc1171ab7f492873
sushma3232 <121096745+sushma3232@users.noreply.github.com>
2023-02-10 10:42:54 +0530

    Add files via upload

---
+++ whatsap.py
@@ -0,0 +1,78 @@
+# def whatsapp():
+#     choose=input("enter the app name")
+#     if choose=="whatsapp":
+#         options=input("enter the options:")
+#         if options=="chats":
+#             names=input("enter the names")
+#             if names=="yedhava":
+#                 print("hi ldc")
+#                 print("how are you")
+#                 print("good")
+#                 print("musuko")
+#             elif names=="pavani":
+#                 print("i want output like this")
+#                 print("do fast")
+#                 print("nice pic")
+#             elif names=="bujjodu":
+#                 print("he is blocked your number")
+#             elif names=="swetha":
+#                 print("sushma niku kovvu vundhi chala")
+#                 print("tallent vundhi ani balupu vunnai")
+#                 print("anichevallu lere")
+#             else:
+#                 print("no chats     ")
+#         elif options=="status":
+#             status_name=input("enter the names:")
+#             if status_name=="my status":
+#                 print("emotional song")
+#             elif status_name=="yedhava":
+#                 print("love songs")
+#             elif status_name=="pavani":
+#                 print("reels")
+#             elif status_name=="swetha":
+#                 print("sodi dailogues")
+#             elif status_name=="nagamani":
+#                 print("deep love")
+#             else:
+#                 print("no status")
+#         elif options=="calls":
+#             print("yedhava vedio call,pavani out going call")
+#         elif options=="profile":
+#             print("nithya ")
+#             print("never put your happyness in someone else hand")
+#         setting_options=input("enter the options:")
+#         if setting_options=="account":
+#             print("privacy,security,changenumber,delete account")
+#             account_choose=input("enter the option:")
+#             if account_choose=="privacy":
+#                 print("last scene,profile,about,status,groups blocked contact")
+#             elif account_choose=="security":
+#                 print("show security notification")
+#             elif account_choose=="change number":
+#                 print("changing your phone number will migrate your account info groupand setting")
+#             elif account_choose=="delete account":
+#                 print("change number instead")
+#         elif setting_options=="chats":
+#             print("thems,wallpaper,chat history")
+#             choose_chats=input("enter option from chat:")
+#             if choose_chats=="themes":
+#                 print("system default,light ,dark")
+#             elif choose_chats=="wallpaper":
+#                 print("wallpaper")
+#             elif choose_chats=="chat history":
+#                 print("export chat,archieve all chat clear all chats delete chats")
+#         elif setting_options=="notifications":
+#             print("vibrate,light,ringtones")
+        
+#         elif setting_options=="storage and data":
+#             print("network usage auto download")
+#         elif setting_options=="help":
+#             print("help center contact us ,privacy policy")
+# def phone():
+#     print("wecome to sushmas phone")
+#     password=input("enter the correct pass word:")
+#     if password=="sushma":
+#         whatsapp()
+#     else:
+#         print("sorry wrong password:")
+# phone()
